ef8fe0a24a4200e3ec554a3b4ac2a3683e3ebb8b
Paula Thomas <paula9t9@gmail.com>
2019-03-31 09:11:28 -0700

    Solved problem 2

--- lab-a/starter-code/app.js
+++ lab-a/starter-code/app.js
@@ -11,7 +11,7 @@ Test this function by hand in the console to get it working, and when you think 
 function sum(a, b) { //eslint-disable-line
 
   var total = a+b;
-  var mathString = 'The sum of ' + a + ' and ' + b + ' is ' + total + '.';
+  var mathString = 'The sum of ' +a+ ' and ' +b+ ' is ' +total+ '.';
   var numberArray = [total, mathString];
 
   return numberArray;
@@ -34,10 +34,18 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
 
+  var product = a*b;
+  var mathString = 'The product of ' +a+ ' and ' +b+ ' is ' +product+ '.';
+  var productArray = [];
+
+  productArray.push(product);
+  productArray.push(mathString);
+
+  return productArray;
 }
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -103,7 +111,7 @@ function multiplyArray(multArr) { //eslint-disable-line
 // Here is the test for multiplyArray(); uncomment it to run it
 // testMultiplyArray(testArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
